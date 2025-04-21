bd2fcfb38a8a657b9178dc948f3357a3344966b8
oskarhs <oskarhsimensen@gmail.com>
2025-04-21 13:01:34 +0200

    Changed default grid procedure to regular for irregular histograms.

--- src/irregular/irregular_histogram.jl
+++ src/irregular/irregular_histogram.jl
@@ -7,7 +7,7 @@ Returns a tuple where the first argument is a StatsBase.Histogram object, the se
 # Arguments
 - `x`: 1D vector of data for which a histogram is to be constructed.
 - `rule`: The criterion used to determine the optimal number of bins. Defaults to the Bayesian method of Simensen et al. (2025).
-- `grid`: String indicating how the finest possible mesh should be constructed. Options are `"data"` (default), which uses each unique data point as a grid point, `"regular"` which constructs a fine regular grid, and `"quantile"` which constructs the grid based on the sample quantiles.
+- `grid`: String indicating how the finest possible mesh should be constructed. Options are `"data"`, which uses each unique data point as a grid point, `"regular"` (default) which constructs a fine regular grid, and `"quantile"` which constructs the grid based on the sample quantiles.
 - `right`: Boolean indicating whether the drawn intervals should be right-inclusive or not. Defaults to `true`.
 - `greedy`: Boolean indicating whether or not the greedy binning strategy of Rozenholc et al. (2006) should be used prior to running the dynamical programming algorithm. Defaults to `true`. The algorithm can be quite slow for large datasets when this keyword is set to `false`.
 - `maxbins`: The maximal number of bins to be considered by the optimization criterion, only used if grid is set to "regular" or "quantile". Defaults to `maxbins=min(4*n/log(n)^2, 1000)`. If the specified argument is not a positive integer, the default value is used.
@@ -24,7 +24,7 @@ julia> H2, criterion2 = histogram_irregular(x; grid="quantile", support=(0.0, 1.
 ```
 ...
 """
-function histogram_irregular(x::AbstractVector{<:Real}; rule::String="bayes", grid::String="data", 
+function histogram_irregular(x::AbstractVector{<:Real}; rule::String="bayes", grid::String="regular", 
                             right::Bool=true, greedy::Bool=true, maxbins::Int=-1, support::Tuple{Real,Real}=(-Inf,Inf),
                             use_min_length::Bool=false, logprior::Function=k->0.0, a::Real=1.0)
     rule = lowercase(rule)
