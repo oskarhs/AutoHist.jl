dcc484b610fb5edfe227dd35ecf64c460c621e1b
oskarhs <oskarhsimensen@gmail.com>
2025-06-15 11:38:00 +0200

    Cleaned up package imports in PlotsExt.

--- ext/PlotsExt.jl
+++ ext/PlotsExt.jl
@@ -2,10 +2,9 @@ module PlotsExt
 
 isdefined(Base, :get_extension) ? (using Plots) : (using ..Plots)
 
-using AutoHist, RecipesBase, StatsBase
-
-# Import package based on version
-#isdefined(Base, :get_extension) ? (using Plots) : (using ..Plots)
+import AutoHist: AutomaticHistogram
+import RecipesBase: @recipe
+import StatsBase: Histogram
 
 # Plot the corresponding StatsBase.Histogram
 @recipe f(::Type{AutomaticHistogram}, h::AutomaticHistogram) = convert(Histogram, h)
